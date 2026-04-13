d89fdfa76cb96b253c5047ca68dd467bf7c45567
Renato Rocha de Souza Junior <56942658+Popilynx@users.noreply.github.com>
2026-04-13 20:55:55 -0300

    chore: update package dependencies and refine type definitions for Astro content modules

--- .astro/actions.d.ts
+++ .astro/actions.d.ts
@@ -1,3 +1,5 @@
 declare module "astro:actions" {
-		export const actions: typeof import("C:/Users/Usuario/Documents/GitHub/PactoAgil/src/actions/index.ts")["server"];
+	type Actions = typeof import("C:/Users/Usuario/Documents/GitHub/PactoAgil/src/actions/index.ts")["server"];
+
+	export const actions: Actions;
 }
